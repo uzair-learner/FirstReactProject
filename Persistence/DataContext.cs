f9c8b16c4d3051cee443b4639c0c03eb89c0284b
uzair-learner <uzairamir7181@gmail.com>
2023-06-10 02:49:12 -0600

    End Of Section 10

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -5,11 +5,10 @@ namespace Persistence
 {
     public class DataContext : DbContext
     {
-         public DataContext(DbContextOptions options) : base(options)
+        public DataContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<Activity> Activities { get; set; }
-    
     }
 }
